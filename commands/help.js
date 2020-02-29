7cdd290d3ccbfad66a70c4fffaf47ec146bd1fd6
Lounès KSOURI <lounes.kmt@icloud.com>
2020-02-29 13:16:00 +0100

    Resolbing bug in help.js

--- commands/help.js
+++ commands/help.js
@@ -1,6 +1,6 @@
 module.exports = {
 	name: 'help',
-	description: 'Obtenir l'aide',
+	description: 'Obtenir l\'aide',
 	execute(message, args) {
     message.channel.send('HELP :      \n \
 > - ``bv [message_id]`` : ajoute les réactions "BIEN VU"\n \
