0bf9cf908158caab128eb0553daf3a5780436373
Lounès KSOURI <lounes.kmt@icloud.com>
2020-03-03 18:40:30 +0100

    Adding "trivial" reaction

---
+++ commands/triv.js
@@ -0,0 +1,19 @@
+module.exports = {
+	name: 'triv',
+	description: 'Trivial',
+	execute(message, args) {
+    message.channel.fetchMessage(args[0])
+      .then(msg => reactTriv(msg))
+      .then(message.delete())
+	},
+};
+
+async function reactTriv(msg) {
+  await msg.react('🇹')
+  await msg.react('🇷')
+  await msg.react('🇮')
+  await msg.react('🇻')
+  await msg.react('🇮')
+  await msg.react('🇦')
+  await msg.react('🇱')
+}
