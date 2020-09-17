ce7e57dcf5a60394ab3ceb14c95edfbfc4664740
Lounès Ksouri <dev@louneskmt.com>
2020-09-17 15:03:32 +0200

    Fixed bug

--- commands/vote.js
+++ commands/vote.js
@@ -16,8 +16,8 @@ module.exports = {
       }
       
       message.channel.send(`Question : ${question}\nChoix : ${choix}\nCible : ${cible}`);
-      const roleId = cible.match(/\d+/g);
-      message.guild.roles.fetch(role)
+      const roleId = cible.match(/\d+/g)[0];
+      message.guild.roles.fetch(roleId)
         .then(role => {
           let members;
           role.members.forEach(member => members.push(member.nickname));
